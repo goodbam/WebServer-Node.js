dfef1e4153d760d014624a587070dffedc94c837
GuBum <wjdrnqja123@gmail.com>
2022-11-09 15:55:25 +0900

    로그인을 요청하는 fetch 생성

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -7,8 +7,18 @@ const loginBtn = document.querySelector("#loginBtn");
 loginBtn.addEventListener("click", login);
 
 function login() {
+  // 서버로 전달할 데이터
   const req = {
     id: id.value,
     psword: psword.value,
   };
+
+  // 서버의 "/login" 경로로 전달
+  fetch("/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(req), // Type 변환 : Object -> String
+  });
 }
