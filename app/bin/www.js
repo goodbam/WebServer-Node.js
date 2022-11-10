6405704da194a1de3fcf0d168e060f1db60fdf4e
GuBum <wjdrnqja123@gmail.com>
2022-11-10 20:56:29 +0900

    로그 관리 모듈 winston 등록 및 설정

--- app/bin/www.js
+++ app/bin/www.js
@@ -2,15 +2,9 @@
 
 //** Import */
 const app = require("../app");
+const logger = require("../src/config/logger");
 const PORT = process.env.SERVER_PORT;
 
 app.listen(PORT, () => {
-  console.log("www : 서버 가동");
+  logger.log("info", `Port ${PORT} -> 서버 실행 `);
 });
-
-// import { listen } from "../app";
-// const PORT = 3000;
-
-// listen(PORT, () => {
-//   console.log("서버 가동");
-// });
---
+++ app/src/config/logger.js
@@ -0,0 +1,80 @@
+"user strict";
+
+const WinstonDaily = require("winston-daily-rotate-file");
+const { createLogger, transports, format } = require("winston");
+const { combine, timestamp, label, printf, json, simple, colorize } = format;
+
+const printFormat = {
+  file: printf(({ label, timestamp, level }) => {
+    return `${timestamp} [${label}] [${level}] :`;
+  }),
+
+  console: printf(({ level, message }) => {
+    return `[${level}] : ${message}`;
+  }),
+};
+
+const printLogFormat = {
+  file: combine(
+    label({
+      label: "GGDuo.com",
+    }),
+    timestamp({
+      format: "YYYY-MM-DD HH:mm:dd",
+    }),
+    printFormat.file
+  ),
+
+  console: combine(
+    colorize(), // 색상 사용
+    simple(),
+    printFormat.console
+  ),
+};
+
+const options = {
+  file: new transports.File({
+    filename: "log.log",
+    dirname: "./logs",
+    level: "info",
+    format: printLogFormat.file,
+  }),
+
+  console: new transports.Console({
+    level: "info",
+    format: printLogFormat.console,
+  }),
+};
+
+const logger = createLogger({
+  transports: [
+    options.file,
+
+    // error 레벨 로그를 저장할 파일 설정
+    new WinstonDaily({
+      level: "error",
+      datePattern: "YYYY-MM-DD",
+      dirname: "./logs/error",
+      filename: "error-%DATE%.log",
+      maxFiles: 30,
+      zippedArchive: true,
+    }),
+
+    // 모든 레벨 로그를 저장할 파일 설정
+    new WinstonDaily({
+      level: "debug",
+      datePattern: "YYYY-MM-DD",
+      dirname: "./logs/all",
+      filename: "all-%DATE%.log",
+      maxFiles: 7,
+      zippedArchive: true,
+    }),
+  ],
+});
+
+// 개발용 서버일 경우 콘솔까지 찍어준다.
+if (process.env.NODE_ENV !== "production") {
+  logger.add(options.console);
+}
+
+module.exports = logger;
